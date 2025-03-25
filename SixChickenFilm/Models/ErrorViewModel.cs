ed64d5b85462e29313765634afe57dea1ca83f33
Nguyen Dinh Thai Ha <thaiha12051311@gmail.com>
2025-03-26 00:15:32 +0700

    Initial commit

---
+++ SixChickenFilm/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SixChickenFilm.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
